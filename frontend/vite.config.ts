dd692b3b7888466180473cd4e0b46c0d69678b15
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-21 17:26:28 +0100

    third commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  test: {
+    coverage: {
+      reporter: ["cobertura", "json", "html"],
+    },
+    environment: "jsdom",
+    globals: true,
+    include: ["src/**/*.{spec,test}.{ts,tsx}"],
+    setupFiles: ["/setupTests.mjs"],
+  },
+});
