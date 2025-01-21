ddada20f8e7299f6c22a891769a61f6059bdd574
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-21 18:30:43 +0100

    prepared db

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,22 +2,34 @@ import { DatabaseSync } from "node:sqlite";
 const db = new DatabaseSync(":memory:");
 
 // Execute SQL statements from strings
-// db.exec(`
-//     CREATE TABLE users (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         username TEXT UNIQUE,
-//         password TEXT
-//     )
-// `);
+db.exec(`
+    CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+`);
 
-// db.exec(`
-//     CREATE TABLE todos (
-//         id INTEGER PRIMARY KEY AUTOINCREMENT,
-//         user_id INTEGER,
-//         task TEXT,
-//         completed BOOLEAN DEFAULT 0,
-//         FOREIGN KEY(user_id) REFERENCES users(id)
-//     )
-// `);
+db.exec(`
+    CREATE TABLE orders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        date TEXT,
+        total REAL,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    )
+`);
+
+db.exec(`
+    CREATE TABLE order_item (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        order_id INTEGER,
+        title TEXT NOT NULL,
+        image TEXT,
+        price REAL NOT NULL,
+        quantity INTEGER NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES orders(id)
+    )
+`);
 
 export default db;
