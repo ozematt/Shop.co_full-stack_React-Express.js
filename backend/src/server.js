9f751140faac277a2cb5440a4000aa7381ba5542
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-22 18:43:48 +0100

    added mysql in project

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 // Konfiguracja połączenia z bazą danych MySQL
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "127.0.0.1",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_DATABASE || "test",
@@ -22,15 +22,15 @@ db.connect((err) => {
   console.log("Connected to MySQL as ID " + db.threadId);
 });
 // Routes
-app.get("/api/users", (req, res) => {
-  db.query("SELECT * FROM users", (err, results) => {
-    if (err) {
-      console.error("Error executing query: " + err.stack);
-      res.status(500).send("Error fetching users");
-      return;
-    }
-    res.json(results);
-  });
+app.get("/", (req, res) => {
+  // db.query("SELECT * FROM users", (err, results) => {
+  //   if (err) {
+  //     console.error("Error executing query: " + err.stack);
+  //     res.status(500).send("Error fetching users");
+  //     return;
+  //   }
+  //   res.json(results);
+  // });
 });
 // Start the server
 app.listen(PORT, () => {
