9afb44dc71cdeafd9f4757b580b98fef4487d935
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-23 14:17:18 +0100

    added containerization for database and backend

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,19 +2,19 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql
-  .createPool({
-    host: process.env.DB_HOST, // Nazwa usługi z docker-compose.yml
-    user: process.env.DB_USER, // Użytkownik bazy danych
-    password: process.env.DB_PASSWORD, // Hasło użytkownika
-    database: process.env.DB_NAME, // Nazwa bazy danych
-    port: process.env.DB_PORT, // Port, na którym działa MySQL w kontenerze
-  })
-  .promise();
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, // Nazwa usługi z docker-compose.yml
+  user: process.env.DB_USER, // Użytkownik bazy danych
+  password: process.env.DB_PASSWORD, // Hasło użytkownika
+  database: process.env.DB_NAME, // Nazwa bazy danych
+  port: process.env.DB_PORT, // Port, na którym działa MySQL w kontenerze
+});
+
+const poolPromise = pool.promise();
 
 const connectToDatabase = async () => {
   try {
-    await pool.getConnection();
+    await poolPromise.getConnection();
     console.log("MySQL Connection Success 👍 👍");
   } catch (error) {
     console.log("Database Connection Error");
