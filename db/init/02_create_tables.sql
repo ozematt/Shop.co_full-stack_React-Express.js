37a303b1362b35fefd2fae120c9f48d9e4e22675
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-28 17:38:26 +0100

    edited files

--- db/init/02_create_tables.sql
+++ db/init/02_create_tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     order_item_id INT,
-    total DECIMAL(10, 2),
+    total DECIMAL(10, 2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -19,8 +19,8 @@ CREATE TABLE IF NOT EXISTS order_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     product_name VARCHAR(255),
-    quantity INT,
-    price DECIMAL(10, 2),
     image VARCHAR(255),
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(id)
 );
