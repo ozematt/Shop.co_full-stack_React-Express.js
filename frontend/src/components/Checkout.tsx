097ff01aebdd13f6dd0bbfa2912e92ac4379e6b4
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-29 14:56:02 +0100

    added dark mode

--- frontend/src/components/Checkout.tsx
+++ frontend/src/components/Checkout.tsx
@@ -136,7 +136,7 @@ const Checkout = () => {
 
                   <button
                     onClick={handleOrder}
-                    className="relative mt-6 w-full max-w-[457px] rounded-full bg-black py-[19px] font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95 max-sm:text-sm"
+                    className="relative mt-6 w-full max-w-[457px] rounded-full bg-black py-[19px] font-satoshi font-medium text-white transition duration-100 ease-in-out hover:scale-95 max-sm:text-sm dark:bg-white dark:text-black"
                   >
                     PAY
                   </button>
