1f4f78a2ae8c29bec13fcc330ea1ab5a6aae9af4
Mateusz Obirek <neutralnie7@gmail.com>
2025-01-29 14:01:49 +0100

    added login dark mode

--- frontend/src/components/LogIn.tsx
+++ frontend/src/components/LogIn.tsx
@@ -61,7 +61,7 @@ const LogIn = () => {
   ////UI
   return (
     <>
-      <section className="max-container bg-grayBG px-4 sm:px-[100px]">
+      <section className="max-container bg-grayBG px-4 sm:px-[100px] dark:bg-zinc-900">
         <div className="mx-auto flex w-full max-w-[400px] flex-col">
           <h2 className="pt-[60px] font-integralCFBold text-[32px] max-sm:leading-9 sm:pt-[100px] sm:text-5xl">
             Welcome Back!
@@ -83,7 +83,7 @@ const LogIn = () => {
                 {...register("username")}
                 type="text"
                 placeholder="Enter your user name"
-                className="h-[48px] w-full rounded-full bg-white pl-[60px] focus:outline-none focus:ring-1 focus:ring-black max-sm:placeholder:text-[14px]"
+                className="h-[48px] w-full rounded-full bg-white pl-[60px] focus:outline-none focus:ring-1 focus:ring-black max-sm:placeholder:text-[14px] dark:bg-zinc-600 dark:focus:ring-2 dark:focus:ring-orange-700"
               />
             </div>
             {errors.username && (
@@ -104,7 +104,7 @@ const LogIn = () => {
                 {...register("password")}
                 type="password"
                 placeholder="Enter your password"
-                className="h-[48px] w-full rounded-full bg-white pl-[60px] focus:outline-none focus:ring-1 focus:ring-black max-sm:placeholder:text-[14px]"
+                className="h-[48px] w-full rounded-full bg-white pl-[60px] focus:outline-none focus:ring-1 focus:ring-black max-sm:placeholder:text-[14px] dark:bg-zinc-600 dark:focus:ring-2 dark:focus:ring-orange-700"
               />
             </div>
             {errors.password && (
